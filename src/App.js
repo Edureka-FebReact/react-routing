934ce9a4cf6da369f651813fcc2f34477f335f0c
ngactive99 <ngactive99@gmail.com>
2022-04-24 21:04:31 +0530

    added home page and error page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 
 import About from "./components/about";
 import Products from "./components/products"
+import ErrorPage from "./components/ErrorPage";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from "./components/home";
 function App() {
   return (
    <Router>
      <nav>
+       <Link to="/">Home</Link>
+       <br/>
        <Link to="/about">About page</Link>
        <br/>
        <Link to="/products">Products page</Link>
      </nav>
      <Routes>
+       <Route path="/" element={<Home/>}/>
        <Route path="/about" element={<About/>}/>
        <Route path="/products" element={<Products/>}/>
+       <Route path="*" element={<ErrorPage/>}/>
      </Routes>
    </Router>
   );
